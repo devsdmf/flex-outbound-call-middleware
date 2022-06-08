42c7d51090e626216511574838b56c3cf3fdfb1d
Lucas Mendes <devsdmf@gmail.com>
2022-06-08 17:34:01 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import * as FlexPlugin from '@twilio/flex-plugin';
+
+import FlexOutboundCallInterceptorPlugin from './FlexOutboundCallInterceptorPlugin';
+
+FlexPlugin.loadPlugin(FlexOutboundCallInterceptorPlugin);
